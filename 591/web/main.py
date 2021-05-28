180a34442a9aeb1b4954c45acb60c99efa7652ec
Jack Pan <jackklpan@gmail.com>
2021-05-28 20:41:03 +0800

    add status to API
    Signed-off-by: Jack Pan <jackklpan@gmail.com>

--- 591/web/main.py
+++ 591/web/main.py
@@ -20,24 +20,31 @@ def read_root(page_size:int,
             last_name:Optional[str]=None,
             poster_sex:Optional[str]=None,
             ):
-    skips = page_size * (page_num - 1)
-    query_dict = {
-        'sex': {'$in': sex},
-        'regionname': {'$in': regionname},
-    }
-    if last_name != None:
-        query_dict['linkman'] = {'$regex': '^{}'.format(last_name)}
-    if phone_number != None:
-        query_dict['phone_number'] = {'$eq': phone_number}
-    if post_by_house_owner != None:
-        if post_by_house_owner == False:
-            query_dict['post_owner'] = {'$ne': '屋主'}
-        else:
-            query_dict['post_owner'] = {'$eq': '屋主'}
-    if poster_sex != None:
-        if poster_sex == '男':
-            query_dict['nick_name'] = {'$regex': '.*先生$'}
-        elif poster_sex == '女':
-            query_dict['nick_name'] = {'$regex': '.*(小姐|太太)$'}
-    result = list(house_col.find(query_dict).skip(skips).limit(page_size))
-    return JSONResponse(result)
+    try:
+        skips = page_size * (page_num - 1)
+        query_dict = {
+            'sex': {'$in': sex},
+            'regionname': {'$in': regionname},
+        }
+        if last_name != None:
+            query_dict['linkman'] = {'$regex': '^{}'.format(last_name)}
+        if phone_number != None:
+            query_dict['phone_number'] = {'$eq': phone_number}
+        if post_by_house_owner != None:
+            if post_by_house_owner == False:
+                query_dict['post_owner'] = {'$ne': '屋主'}
+            else:
+                query_dict['post_owner'] = {'$eq': '屋主'}
+        if poster_sex != None:
+            if poster_sex == '男':
+                query_dict['nick_name'] = {'$regex': '.*先生$'}
+            elif poster_sex == '女':
+                query_dict['nick_name'] = {'$regex': '.*(小姐|太太)$'}
+        result = list(house_col.find(query_dict).skip(skips).limit(page_size))
+        return JSONResponse({
+                'status': 'success',
+                'data': result})
+    except Exception as e:
+        return JSONResponse({
+                'status': 'failed',
+                'error': str(e)})
